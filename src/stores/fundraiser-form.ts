0d3b3ce2dbbc710b570dbc3e60ab8ecb3c790483
Tech Tafari <mpiers110@gmail.com>
2025-06-28 04:33:44 +0300

    refactor: update fundraiser form state management for clarity and consistency

--- src/stores/fundraiser-form.ts
+++ src/stores/fundraiser-form.ts
@@ -1,17 +1,22 @@
-// import { IndividualFundraiserFormData } from '@components/workflows/FundariserForms/individualForm';
-// import { OrganizationFundraiserFormData } from '@components/workflows/FundariserForms/OrganizationForm';
-// import { StartupFundraiserFormData } from '@components/workflows/FundariserForms/startupForm';
+import { IndividualFundraiserFormData } from '@components/workflows/FundariserForms/individualForm';
+import { OrganizationFundraiserFormData } from '@components/workflows/FundariserForms/OrganizationForm';
+import { StartupFundraiserFormData } from '@components/workflows/FundariserForms/startupForm';
 import { create } from 'zustand';
+
+
+export type FundraiserData =
+  | IndividualFundraiserFormData
+  | StartupFundraiserFormData
+  | OrganizationFundraiserFormData;
 
 interface FundraiserTypeState {
   fundraiserType: string | null;
-  fundraiserDetails: string | null;
-  // IndividualFundraiserFormData | StartupFundraiserFormData | OrganizationFundraiserFormData | null;
+  fundraiserData: FundraiserData | null;
   setFundraiserType: (type: string) => void;
   setFundraiserDetails: (details: string) => void;
-  // (details: IndividualFundraiserFormData | StartupFundraiserFormData | OrganizationFundraiserFormData) => void;
+  setFundraiserData: (data: FundraiserData) => void;
 }
-const fundraiserDetails = 
+export const fundraiserDetails = 
     `Help Sarah Johnson recover from a life-changing accident.
     Sarah, a devoted mother of two and elementary school teacher,
     was involved in a serious car accident that has left her with multiple injuries requiring extensive medical treatment and rehabilitation.
@@ -22,7 +27,20 @@ const fundraiserDetails =
 
 export const useFundraiserTypeStore = create<FundraiserTypeState>((set) => ({
   fundraiserType: null,
-  fundraiserDetails: fundraiserDetails || null,
-  setFundraiserDetails: (details) => set({ fundraiserDetails: details }),
+  fundraiserData: null,
+
   setFundraiserType: (type) => set({ fundraiserType: type }),
-})); 
+
+  setFundraiserDetails: (details) =>
+    set((state) => {
+      if (!state.fundraiserData) {
+        console.warn("Trying to set details, but fundraiserData is null");
+        return state;
+      }
+      return {
+        fundraiserData: { ...state.fundraiserData, details },
+      };
+    }),
+
+  setFundraiserData: (data) => set({ fundraiserData: data }),
+}));
